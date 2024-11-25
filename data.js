c6f074a9a4bdfe39ec8d29c76d5db323205c3c0f
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-11-25 16:33:41 +0530

    list of restuarants

---
+++ data.js
@@ -0,0 +1,206 @@
+const ResList = [
+    {
+      id: "336912",
+      name: "Pizza Hut",
+      locality: "Selvapalayam",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      totalRatingsString: "3.4K+",
+      cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/5825f6bc-7734-4670-b72a-3731db6edb62_336912.jpg"
+    },
+    {
+      id: "328821",
+      name: "KFC",
+      locality: "Kajas",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
+      avgRating: 4.4,
+      totalRatingsString: "7.3K+",
+      cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/5/d016bb44-536a-46d6-b128-2f99f899365d_328821.JPG"
+    },
+    {
+      id: "169881",
+      name: "Domino's Pizza",
+      locality: "Palakkad",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas"],
+      avgRating: 4.3,
+      totalRatingsString: "1.9K+",
+      cloudinaryImageId: "023a63cc663b328750810b48abd228b4"
+    },
+    {
+      id: "101001",
+      name: "Burger King",
+      locality: "South Market",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "Fast Food"],
+      avgRating: 4.1,
+      totalRatingsString: "5K+",
+      cloudinaryImageId: "l8utpynxvsn18yzzialp"
+    },
+    {
+      id: "101002",
+      name: "Subway",
+      locality: "Greenway Avenue",
+      costForTwo: "₹300 for two",
+      cuisines: ["Sandwiches", "Healthy Food"],
+      avgRating: 4.2,
+      totalRatingsString: "2.1K+",
+      cloudinaryImageId: "f1bc9ddf53de574cdc35ab2f717df234"
+    },
+    {
+      id: "101003",
+      name: "Taco Bell",
+      locality: "North Square",
+      costForTwo: "₹400 for two",
+      cuisines: ["Mexican", "Fast Food"],
+      avgRating: 4.0,
+      totalRatingsString: "1.8K+",
+      cloudinaryImageId: "lb5o4kl1cxikwxpqka6a"
+    },
+    {
+      id: "101004",
+      name: "Starbucks",
+      locality: "Central Mall",
+      costForTwo: "₹500 for two",
+      cuisines: ["Coffee", "Snacks"],
+      avgRating: 4.5,
+      totalRatingsString: "3.2K+",
+      cloudinaryImageId: "djodmmxum2curc7agsp1"
+    },
+    {
+      id: "101005",
+      name: "McDonald's",
+      locality: "Main Street",
+      costForTwo: "₹250 for two",
+      cuisines: ["Burgers", "Desserts"],
+      avgRating: 4.2,
+      totalRatingsString: "8.4K+",
+      cloudinaryImageId: "10e47ca50f6b4106d398818aaf4f5a73"
+    },
+    {
+      id: "101006",
+      name: "Haldiram's",
+      locality: "City Plaza",
+      costForTwo: "₹300 for two",
+      cuisines: ["Indian", "Sweets"],
+      avgRating: 4.4,
+      totalRatingsString: "4.6K+",
+      cloudinaryImageId: "dyy2e2kstsqbwny69up2"
+    },
+    {
+      id: "101007",
+      name: "Biryani Blues",
+      locality: "East Road",
+      costForTwo: "₹500 for two",
+      cuisines: ["Biryani", "Mughlai"],
+      avgRating: 4.3,
+      totalRatingsString: "6.2K+",
+      cloudinaryImageId: "w01lvrofquazbt49kpjs"
+    },
+    {
+      id: "101008",
+      name: "Wow! Momo",
+      locality: "Mall Road",
+      costForTwo: "₹250 for two",
+      cuisines: ["Momoz", "Snacks"],
+      avgRating: 4.0,
+      totalRatingsString: "1.5K+",
+      cloudinaryImageId: "nbcyjurq2qoxdztvibxg"
+    },
+    {
+      id: "101009",
+      name: "Dunkin'",
+      locality: "Coffee Square",
+      costForTwo: "₹350 for two",
+      cuisines: ["Coffee", "Desserts"],
+      avgRating: 4.1,
+      totalRatingsString: "2.3K+",
+      cloudinaryImageId: "lcuqpu4x67sbf5prbqcg"
+    },
+    {
+      id: "101010",
+      name: "Costa Coffee",
+      locality: "Town Hall",
+      costForTwo: "₹400 for two",
+      cuisines: ["Coffee", "Snacks"],
+      avgRating: 4.2,
+      totalRatingsString: "1.9K+",
+      cloudinaryImageId: "xon4kleydyt3noltdtts"
+    },
+    {
+      id: "101011",
+      name: "Barbeque Nation",
+      locality: "Central Avenue",
+      costForTwo: "₹1200 for two",
+      cuisines: ["Grill", "Buffet"],
+      avgRating: 4.6,
+      totalRatingsString: "7.8K+",
+      cloudinaryImageId: "iqphgvfrleoiogwb8i2d"
+    },
+    {
+      id: "101012",
+      name: "Paradise Biryani",
+      locality: "Food Court",
+      costForTwo: "₹600 for two",
+      cuisines: ["Biryani", "Indian"],
+      avgRating: 4.3,
+      totalRatingsString: "6.4K+",
+      cloudinaryImageId: "btzcwvpiwdbn4vo4aeuj"
+    },
+    {
+      id: "101013",
+      name: "Moti Mahal",
+      locality: "Old Town",
+      costForTwo: "₹800 for two",
+      cuisines: ["North Indian", "Tandoori"],
+      avgRating: 4.4,
+      totalRatingsString: "3.2K+",
+      cloudinaryImageId: "djodmmxum2curc7agsp1"
+    },
+    {
+      id: "101014",
+      name: "Punjab Grill",
+      locality: "Golden Square",
+      costForTwo: "₹900 for two",
+      cuisines: ["North Indian", "Punjabi"],
+      avgRating: 4.3,
+      totalRatingsString: "4.1K+",
+      cloudinaryImageId: "f1bc9ddf53de574cdc35ab2f717df234"
+    },
+    {
+      id: "101015",
+      name: "Indian Accent",
+      locality: "City Centre",
+      costForTwo: "₹1500 for two",
+      cuisines: ["Indian", "Modern"],
+      avgRating: 4.8,
+      totalRatingsString: "2.3K+",
+      cloudinaryImageId: "l8utpynxvsn18yzzialp"
+    },
+    {
+      id: "101016",
+      name: "Le Cirque",
+      locality: "Luxury Lane",
+      costForTwo: "₹3000 for two",
+      cuisines: ["French", "Italian"],
+      avgRating: 4.9,
+      totalRatingsString: "1.7K+",
+      cloudinaryImageId: "lb5o4kl1cxikwxpqka6a"
+    },
+    {
+      id: "101017",
+      name: "Chili's",
+      locality: "Mall Plaza",
+      costForTwo: "₹800 for two",
+      cuisines: ["Tex-Mex", "American"],
+      avgRating: 4.2,
+      totalRatingsString: "3.6K+",
+      cloudinaryImageId: "023a63cc663b328750810b48abd228b4"
+    }
+  ];
+  
+  
+
+export default ResList;
